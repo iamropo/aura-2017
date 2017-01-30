a182f16c0234d934b67d6feecf661b2416986796
iamropo <iamropo@gmail.com>
2017-01-30 22:27:23 +0530

    navigation bar background color change

--- js/navigation-bar.js
+++ js/navigation-bar.js
@@ -1,5 +1,15 @@
 var navigationBar = document.querySelector('nav')
 
+window.addEventListener('scroll', function () {
+	var mainHeight = document.querySelector('#main').offsetHeight
+	if (window.scrollY >= mainHeight) {
+		navigationBar.style.backgroundColor = 'rgba(0, 0, 0, 0.8)'
+	} else {
+		navigationBar.style.backgroundColor = 'rgba(0, 0, 0, 0.2)'
+	}
+
+})
+
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
@@ -13,4 +23,4 @@ $(function() {
       }
     }
   });
-});
+});	
